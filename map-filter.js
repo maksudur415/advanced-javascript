83cd12815cbfdb4d220447d1649bfb772d7496a1
maksudur415 <rahman.maksudur415@gmail.com>
2021-12-07 00:07:58 +0800

    map-filter

---
+++ map-filter.js
@@ -0,0 +1,17 @@
+const numbers = [3, 4, 5, 6, 7, 8];
+// const output = [];
+function square (element){
+    return element * element;
+}
+// numbers.map(function(element)){
+//     console.log(element, index, array);
+// }
+// numbers.map(function(element, index, array){
+//     console.log(element, index, array);
+// })
+// const result = numbers.map(function(element){
+//     return element * element;
+// })
+
+const bigger = numbers.filter( x => x < 5);
+console.log(result);
